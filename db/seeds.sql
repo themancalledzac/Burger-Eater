4f848f6686754869d5bfcabcf1453db3e09c8b68
Zac Edens <themancalledzac@gmail.com>
2021-01-07 17:35:03 -0800

    seeds.sql file updated.

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burger_db;
+INSERT INTO
+  burgers (burger_name)
+VALUES
+  ("California Ave");
+INSERT INTO
+  burgers (burger_name)
+VALUES
+  ("Teriyaki Tower");
+INSERT INTO
+  burgers (burger_name)
+VALUES
+  ("The Classic");
