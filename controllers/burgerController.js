983350419b595e5ec62a77fbb2014dbda2156caf
Zac Edens <themancalledzac@gmail.com>
2021-01-11 12:20:29 -0800

    trying to get my initial get route to work, but running into an error. Committing progress so far.

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -2,16 +2,31 @@ var express = require("express");
 
 var router = express.Router();
 
+// import the model (burger.js) to use its database functions.
 var burger = ("../models/burger.js");
 
+// Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-    burger.localeCompare(function (data) {
-        var object = {
+    burger.all(function (data) {
+        var hbsObject = {
             burgers: data
         };
-        console.log(object);
-        res.render("index", object);
+        console.log(hbsObject);
+        res.render("index", hbsObject);
     });
 });
+
+router.post("/api/burgers", function (req, res) {
+    burger.create(
+        [
+            "burger_name", "burger_description"
+        ], // not ssure if req.body.name or not?
+        [req.body.burger_name, req.body.burger_description], function (result) {
+            res.json({ id: result.insertId });
+        }
+    );
+});
+
+router.put("/api/burgers/:burger_id")
 
 module.exports = router;
