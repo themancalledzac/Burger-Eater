2341273eb3136f048a865df127bb72142b09ac68
Zac Edens <themancalledzac@gmail.com>
2021-01-11 14:45:15 -0800

    simplified the schema and seeds files for heroku upload issues.

--- db/schema.sql
+++ db/schema.sql
@@ -13,51 +13,51 @@ CREATE TABLE burgers (
   current_menu BOOLEAN DEFAULT false,
   PRIMARY KEY (burger_id)
 );
-USE burger_db;
-CREATE TABLE toppings (
-  topping_id INT AUTO_INCREMENT,
-  topping_name VARCHAR(30) NOT NULL,
-  topping_price DECIMAL(4, 2),
-  topping_stock INT,
-  PRIMARY KEY (topping_id)
-);
-USE burger_db;
-CREATE TABLE burger_toppings (
-  burger_id int NOT NULL,
-  topping_id int NOT NULL,
-  PRIMARY KEY (burger_id, topping_id),
-  FOREIGN KEY (burger_id) REFERENCES burgers(burger_id),
-  FOREIGN KEY (topping_id) REFERENCES toppings(topping_id)
-);
-USE burger_db;
-CREATE TABLE condiments (
-  condiment_id INT AUTO_INCREMENT,
-  condiment_name VARCHAR(20) NOT NULL,
-  condiment_price DECIMAL (4, 2),
-  condiment_stock INT,
-  PRIMARY KEY (condiment_id)
-);
-USE burger_db;
-CREATE TABLE burger_condiments (
-  burger_id int NOT NULL,
-  condiment_id int NOT NULL,
-  PRIMARY KEY (burger_id, condiment_id),
-  FOREIGN KEY (burger_id) REFERENCES burgers(burger_id),
-  FOREIGN KEY (condiment_id) REFERENCES condiments(condiment_id)
-);
-USE burger_db;
-CREATE TABLE proteins (
-  prot_id INT AUTO_INCREMENT,
-  protein_name VARCHAR(30) NOT NULL,
-  protein_price DECIMAL (5, 2),
-  protein_stock INT,
-  PRIMARY KEY (prot_id)
-);
-USE burger_db;
-CREATE TABLE burger_proteins (
-  burger_id int NOT NULL,
-  prot_id int NOT NULL,
-  PRIMARY KEY (burger_id, prot_id),
-  FOREIGN KEY (burger_id) REFERENCES burgers(burger_id),
-  FOREIGN KEY (prot_id) REFERENCES proteins(prot_id)
-);
+-- USE burger_db;
+-- CREATE TABLE toppings (
+--   topping_id INT AUTO_INCREMENT,
+--   topping_name VARCHAR(30) NOT NULL,
+--   topping_price DECIMAL(4, 2),
+--   topping_stock INT,
+--   PRIMARY KEY (topping_id)
+-- );
+-- USE burger_db;
+-- CREATE TABLE burger_toppings (
+--   burger_id int NOT NULL,
+--   topping_id int NOT NULL,
+--   PRIMARY KEY (burger_id, topping_id),
+--   FOREIGN KEY (burger_id) REFERENCES burgers(burger_id),
+--   FOREIGN KEY (topping_id) REFERENCES toppings(topping_id)
+-- );
+-- USE burger_db;
+-- CREATE TABLE condiments (
+--   condiment_id INT AUTO_INCREMENT,
+--   condiment_name VARCHAR(20) NOT NULL,
+--   condiment_price DECIMAL (4, 2),
+--   condiment_stock INT,
+--   PRIMARY KEY (condiment_id)
+-- );
+-- USE burger_db;
+-- CREATE TABLE burger_condiments (
+--   burger_id int NOT NULL,
+--   condiment_id int NOT NULL,
+--   PRIMARY KEY (burger_id, condiment_id),
+--   FOREIGN KEY (burger_id) REFERENCES burgers(burger_id),
+--   FOREIGN KEY (condiment_id) REFERENCES condiments(condiment_id)
+-- );
+-- USE burger_db;
+-- CREATE TABLE proteins (
+--   prot_id INT AUTO_INCREMENT,
+--   protein_name VARCHAR(30) NOT NULL,
+--   protein_price DECIMAL (5, 2),
+--   protein_stock INT,
+--   PRIMARY KEY (prot_id)
+-- );
+-- USE burger_db;
+-- CREATE TABLE burger_proteins (
+--   burger_id int NOT NULL,
+--   prot_id int NOT NULL,
+--   PRIMARY KEY (burger_id, prot_id),
+--   FOREIGN KEY (burger_id) REFERENCES burgers(burger_id),
+--   FOREIGN KEY (prot_id) REFERENCES proteins(prot_id)
+-- );
--- db/seeds.sql
+++ db/seeds.sql
@@ -57,137 +57,137 @@ VALUES
     "13.99"
   );
 -- toppings inert
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("bun", "0.50", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("onion", "0.20", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("pickle", "0.10", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("lettuce", "0.20", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("coleslaw", "0.20", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("kimchi", "0.20", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("arugula", "0.20", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("tomato", "0.30", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("avocado", "1.0", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("bacon", "1.20", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("mushrooms", "0.30", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("egg", "0.80", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("American Cheese", "0.30", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("Swiss Cheese", "0.50", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("Pepper-Jack Cheese", "0.40", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("Sharp Cheddar Cheese", "0.70", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("Gouda Cheese", "0.80", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("Cream Cheese", "0.30", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("Mozzarella Cheese", "0.40", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("Goat Cheese", "0.80", "100");
-INSERT INTO
-  toppings (topping_name, topping_price, topping_stock)
-VALUES
-  ("Blue Cheese", "0.90", "100");
--- condiment entries
-INSERT INTO
-  condiments (condiment_name, condiment_price, condiment_stock)
-VALUES
-  ("mayo", "0.1", "5000");
-INSERT INTO
-  condiments (condiment_name, condiment_price, condiment_stock)
-VALUES
-  ("ketchup", "0.1", "5000");
-INSERT INTO
-  condiments (condiment_name, condiment_price, condiment_stock)
-VALUES
-  ("mustard", "0.1", "5000");
-INSERT INTO
-  condiments (condiment_name, condiment_price, condiment_stock)
-VALUES
-  ("relish", "0.2", "3000");
-INSERT INTO
-  condiments (condiment_name, condiment_price, condiment_stock)
-VALUES
-  ("special sauce", "0.2", "5000");
-INSERT INTO
-  condiments (condiment_name, condiment_price, condiment_stock)
-VALUES
-  ("Teriyaki", "0.2", "5000");
--- protein entries
-INSERT INTO
-  proteins (protein_name, protein_price, protein_stock)
-VALUES
-  ("Ground Beef", "3.0", "50");
-INSERT INTO
-  proteins (protein_name, protein_price, protein_stock)
-VALUES
-  ("Ground Pork", "3.5", "50");
-INSERT INTO
-  proteins (protein_name, protein_price, protein_stock)
-VALUES
-  ("Ground Lamb", "4.5", "50");
-INSERT INTO
-  proteins (protein_name, protein_price, protein_stock)
-VALUES
-  ("Impossible", "6.5", "50");
-INSERT INTO
-  proteins (protein_name, protein_price, protein_stock)
-VALUES
-  ("Chicken Breast", "4.0", "50");
-INSERT INTO
-  proteins (protein_name, protein_price, protein_stock)
-VALUES
-  ("Steak", "10.0", "20");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("bun", "0.50", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("onion", "0.20", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("pickle", "0.10", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("lettuce", "0.20", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("coleslaw", "0.20", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("kimchi", "0.20", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("arugula", "0.20", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("tomato", "0.30", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("avocado", "1.0", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("bacon", "1.20", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("mushrooms", "0.30", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("egg", "0.80", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("American Cheese", "0.30", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("Swiss Cheese", "0.50", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("Pepper-Jack Cheese", "0.40", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("Sharp Cheddar Cheese", "0.70", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("Gouda Cheese", "0.80", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("Cream Cheese", "0.30", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("Mozzarella Cheese", "0.40", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("Goat Cheese", "0.80", "100");
+  -- INSERT INTO
+  --   toppings (topping_name, topping_price, topping_stock)
+  -- VALUES
+  --   ("Blue Cheese", "0.90", "100");
+  -- -- condiment entries
+  -- INSERT INTO
+  --   condiments (condiment_name, condiment_price, condiment_stock)
+  -- VALUES
+  --   ("mayo", "0.1", "5000");
+  -- INSERT INTO
+  --   condiments (condiment_name, condiment_price, condiment_stock)
+  -- VALUES
+  --   ("ketchup", "0.1", "5000");
+  -- INSERT INTO
+  --   condiments (condiment_name, condiment_price, condiment_stock)
+  -- VALUES
+  --   ("mustard", "0.1", "5000");
+  -- INSERT INTO
+  --   condiments (condiment_name, condiment_price, condiment_stock)
+  -- VALUES
+  --   ("relish", "0.2", "3000");
+  -- INSERT INTO
+  --   condiments (condiment_name, condiment_price, condiment_stock)
+  -- VALUES
+  --   ("special sauce", "0.2", "5000");
+  -- INSERT INTO
+  --   condiments (condiment_name, condiment_price, condiment_stock)
+  -- VALUES
+  --   ("Teriyaki", "0.2", "5000");
+  -- -- protein entries
+  -- INSERT INTO
+  --   proteins (protein_name, protein_price, protein_stock)
+  -- VALUES
+  --   ("Ground Beef", "3.0", "50");
+  -- INSERT INTO
+  --   proteins (protein_name, protein_price, protein_stock)
+  -- VALUES
+  --   ("Ground Pork", "3.5", "50");
+  -- INSERT INTO
+  --   proteins (protein_name, protein_price, protein_stock)
+  -- VALUES
+  --   ("Ground Lamb", "4.5", "50");
+  -- INSERT INTO
+  --   proteins (protein_name, protein_price, protein_stock)
+  -- VALUES
+  --   ("Impossible", "6.5", "50");
+  -- INSERT INTO
+  --   proteins (protein_name, protein_price, protein_stock)
+  -- VALUES
+  --   ("Chicken Breast", "4.0", "50");
+  -- INSERT INTO
+  --   proteins (protein_name, protein_price, protein_stock)
+  -- VALUES
+  --   ("Steak", "10.0", "20");
