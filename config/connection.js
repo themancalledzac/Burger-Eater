aec91ae2d5090da9c8d181b82b9a8919f2b01dfe
Zac Edens <themancalledzac@gmail.com>
2021-01-11 16:18:36 -0800

    added JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,18 @@ const mysql = require("mysql");
 const util = require("util");
 
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burger_db"
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burger_db"
+    });
+}
 
 // Make connection.
 connection.connect(function (err) {
